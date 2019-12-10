ea0aef1ccf9e9620b7220b82bb1c95ef5d09bf9c
Juan Verduzco <jverduzco046@west-mec.org>
2019-12-09 18:09:22 -0700

    event listener

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 const input = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 
+
 const expand = () => {
+
   searchBtn.classList.toggle("close");
+
   input.classList.toggle("square");
 };
 
+
 searchBtn.addEventListener("click", expand);
 
+
 // scroll things
+document.addEventListener(scrollY, function () {
+document.getElementById("slogan_content_inner").innerHTML.style.opacity = 0;
+});
